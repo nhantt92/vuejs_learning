6d29c022019982ee32e8859fcc20ac58aedb6da0
nhantt <nhanttn92@gmail.com>
2023-03-02 22:21:19 +0700

    HelloWorld

---
+++ HelloWorld/src/App.vue
@@ -0,0 +1,15 @@
+<!--
+  Say Hello World with Vue!
+-->
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+<template>
+  <h1>{{ message }}</h1>
+</template>
