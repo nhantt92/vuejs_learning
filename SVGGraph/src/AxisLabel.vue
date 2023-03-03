6885a257d85be2b90d82503816873e5b0448993a
nhantt <nhanttn92@gmail.com>
2023-03-03 15:21:41 +0700

    SVG Graph

---
+++ SVGGraph/src/AxisLabel.vue
@@ -0,0 +1,20 @@
+<script>
+import { valueToPoint } from './util.js'
+
+export default {
+    props: {
+        stat: Object,
+        index: Number,
+        total: Number
+    }, 
+    computed: {
+        point: function() {
+            return valueToPoint(+this.stat.value + 10, this.index, this.total)
+        }
+    }
+}
+</script>
+
+<template>
+    <text :x="point.x" :y="point.y">{{ stat.label }}</text>
+    </template>
