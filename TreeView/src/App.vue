5f7613d00affc23a7465459932b077ca6151f078
nhantt <nhanttn92@gmail.com>
2023-03-03 13:41:30 +0700

    TreeView

---
+++ TreeView/src/App.vue
@@ -0,0 +1,56 @@
+<!--
+A nested tree component that recursively renders itself.
+You can double click on an item to
+-->
+<script>
+import TreeItem from "./TreeItem.vue";
+
+const treeData = {
+  name: "My Tree",
+  children: [
+    { name: "hello" },
+    { name: "wat" },
+    {
+      name: "child folder",
+      children: [
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }],
+        },
+        { name: "hello" },
+        { name: "wat" },
+        {
+          name: "child folder",
+          children: [{ name: "hello" }, { name: "wat" }],
+        },
+      ],
+    },
+  ],
+};
+
+export default {
+  components: {
+    TreeItem,
+  },
+  data() {
+    return {
+      treeData,
+    };
+  },
+};
+</script>
+<template>
+  <ul>
+    <TreeItem class="item" :model="treeData"></TreeItem>
+  </ul>
+</template>
+
+<style>
+.item {
+  cursor: pointer;
+  line-height: 1.5;
+}
+.bold {
+  font-weight: bold;
+}
+</style>
