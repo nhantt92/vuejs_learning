63c15469ab9e3248b9d6a9a38cd5fd39a2e6fdba
nhantt <nhanttn92@gmail.com>
2023-03-04 09:59:13 +0700

    Counter

---
+++ Counter/src/App.vue
@@ -0,0 +1,21 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#counter
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  watch() {
+    
+  }
+}
+</script>
+
+<template>
+<button @click="count++">Count</button>
+<p>{{  count }}</p> 
+</template>
